6e67ec2e0416609f565adebc344b967e3f9f6801
youflox <58954924+youflox@users.noreply.github.com>
2020-02-17 16:37:43 +0530

    Update models.py
    Adding names to database

--- weather_app/models.py
+++ weather_app/models.py
@@ -4,6 +4,16 @@ from weather_app import app
 import requests
 url =  'http://api.openweathermap.org/data/2.5/weather?appid=41e49a44177d59caffa315f4fc69de5f&q='
 
+#-------------------------------   DATABASE ---------------------------------------------------------------------
+
+class Citynames(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    city = db.column(db.String, nullable = False)
+    
+    
+    
+#-------------------------------  Operationn on API  ---------------------------------------------------------------------
+
 def check(name):
     try:
         weather = []
@@ -11,6 +21,8 @@ def check(name):
         types = ['temp','feels_like', 'temp_min', 'temp_max']
         check_url = url+name
         r = requests.get(check_url).json()
+        
+        name = Citynames(city = name)
 
         def cel(temp):
             return int(temp)-273.15
@@ -41,8 +53,3 @@ def check(name):
     except:
         return 'City not found, try another one.'
 
-#-------------------------------   DATABASE ---------------------------------------------------------------------
-
-class citynames(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    city = db.column(db.String, nullable = False)
